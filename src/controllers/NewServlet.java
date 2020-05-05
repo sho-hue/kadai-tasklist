92dea441fe3dc6d38797d73f302bd5793137661b
shota.matsuyama <shomtym1@gmail.com>
2020-05-05 22:01:31 +0900

    Modify Index/NewServlet

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,9 +1,8 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
-import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,8 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
-import utils.DBUtil;
-
 
 /**
  * Servlet implementation class NewServlet
@@ -35,7 +32,7 @@ public class NewServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 
-			EntityManager em = DBUtil.createEntityManager();
+/*			EntityManager em = DBUtil.createEntityManager();
 			em.getTransaction().begin();
 
 			Task t = new Task();
@@ -53,5 +50,12 @@ public class NewServlet extends HttpServlet {
 
 			response.getWriter().append(Integer.valueOf(t.getId()).toString());
 			em.close();
-}
+*/
+		request.setAttribute("_token",request.getSession().getId());
+
+		request.setAttribute("task",new Task());
+
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+		rd.forward(request,response);
+	}
 }
